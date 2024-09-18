bba83b0179df24fea24feec2e092e4351715f1dc
brandishcode <brandishcode@gmail.com>
2024-09-18 21:45:06 +0900

    Enable treesitter folding

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -3,4 +3,10 @@
     enable = true;
     settings.highlight = { enable = true; };
   };
+
+  extraConfigVim = ''
+    set foldmethod=expr
+    set foldexpr=nvim_treesitter#foldexpr()
+    set nofoldenable
+  '';
 }
