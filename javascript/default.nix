1903a73e3c3abe46319284c361a694c263587197
brandishcode <brandishcode@gmail.com>
2024-10-14 15:29:53 +0900

    Allow prettierd to work on javascriptreact filetypes

--- javascript/default.nix
+++ javascript/default.nix
@@ -5,6 +5,9 @@ in {
   extraPackages = [ pkgs.${javascriptformatter} ];
   plugins.lsp.servers.ts_ls.enable = true;
   plugins.conform-nvim = {
-    settings.formatters_by_ft = { javascript = [ javascriptformatter ]; };
+    settings.formatters_by_ft = {
+      javascript = [ javascriptformatter ];
+      javascriptreact = [ javascriptformatter ];
+    };
   };
 }
