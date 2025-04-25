73183966ff1caa77bfb4d56972cb638380546c1c
brandishcode <brandishcode@gmail.com>
2025-04-25 21:59:14 +0900

    Added lua lsp

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -1,14 +1,19 @@
 { pkgs, ... }:
 
-let nixformatter = "nixfmt";
+let
+  nixformatter = "nixfmt";
+  luaformatter = "luaformatter";
 in {
-  extraPackages = [ pkgs.${nixformatter} ];
+  extraPackages = [ pkgs.${nixformatter} pkgs.${luaformatter} ];
   plugins.conform-nvim = {
     enable = true;
 
     settings = {
       format_on_save = { timeoutMs = 500; };
-      formatters_by_ft = { nix = [ nixformatter ]; };
+      formatters_by_ft = {
+        nix = [ nixformatter ];
+        lua = [ luaformatter ];
+      };
     };
   };
 }
--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -16,6 +16,7 @@
       servers = {
         nixd.enable = true;
         marksman.enable = true;
+        lua_ls.enable = true;
       };
     };
     colorizer.enable = true;
