58a42b6ac10ec64991bd4ea80c66ab1dbfab623f
brandishcode <brandishcode@gmail.com>
2025-06-05 21:55:12 +0900

    conform: manual format of file instead on save

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -5,13 +5,24 @@ in {
   extraPackages = [ pkgs.${nixformatter} pkgs.luaformatter ];
   plugins.conform-nvim = {
     enable = true;
-
-    settings = {
-      format_on_save = { timeoutMs = 500; };
-      formatters_by_ft = {
-        nix = [ nixformatter ];
-        lua = [ "lua-format" ];
-      };
-    };
+    luaConfig.post = ''
+      vim.api.nvim_create_user_command("Format", function(args)
+        local range = nil
+        if args.count ~= -1 then
+          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+          range = {
+            start = { args.line1, 0 },
+            ["end"] = { args.line2, end_line:len() },
+          }
+        end
+        require("conform").format({ async = true, lsp_format = "fallback", range = range })
+      end, { range = true })
+    '';
   };
+  keymaps = [{
+    mode = "n";
+    action = "<Cmd>Format<Cr>";
+    key = "<Leader>ft";
+  }];
+
 }
